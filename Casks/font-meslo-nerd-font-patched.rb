2220d2bcbff2bb26d589c15ea4daec676c277e42
issenn <issennknight@gmail.com>
2020-03-29 04:29:02 +0800

    Initial commit

--- Casks/font-meslo-nerd-font-patched.rb
+++ Casks/font-meslo-nerd-font-patched.rb
@@ -1,6 +1,6 @@
 cask 'font-meslo-nerd-font-patched' do
   version '0.0.1'
-  sha256 '7cf4e4c6289e2685ab7215019cb4b21e9503ea97898a64f7e9c46e4f08cc0407'
+  sha256 '53a25d4eb811df23c67e88bc8a2d31124e4f032cfcecc61151dfce0d736bdf49'
   # url "https://github.com/romkatv/powerlevel10k-media.git", :using => :git, :branch => 'master'
   url "https://github.com/romkatv/powerlevel10k-media/archive/master.zip"
   appcast 'https://github.com/romkatv/powerlevel10k-media/releases.atom'
